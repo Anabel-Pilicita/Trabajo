79145db82173561710300550b459d02896eff887
Anabel-Pilicita <anabelpilicita@gmail.com>
2016-06-24 18:51:53 +0200

    Revert "Páginas Web"
    This reverts commit fbd259defb24edad697a04077983a9d40f237860.

--- links.php
+++
@@ -1,7 +0,0 @@
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-144-precomposed.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114-precomposed.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72-precomposed.png" />
-	<link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png" />
-	<link rel="shortcut icon" href="img/favicon.png" />
-	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
